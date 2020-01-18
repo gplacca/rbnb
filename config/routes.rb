3e2cf67c1a343254efaa3bc2c614d41b7b373d26
gregoire placca <gregoire.placca@hotmail.com>
2020-01-18 12:58:04 +0100

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :infrastructures
   get 'joke' => 'pages#joke', as: :joke
-  get 'index' => 'infrastructures#index', as: :index
-  get 'show' => 'infrastructures#show', as: :show
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
