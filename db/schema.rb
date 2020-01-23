2ab8b16c66e4a27a3585e6bad19b0169fb6e4af5
Alvaro Vega <nunyvega@hotmail.com>
2020-01-23 23:02:53 +0100

    change all local media sources to web hosted files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_065921) do
+ActiveRecord::Schema.define(version: 2020_01_23_082954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_01_17_065921) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "image"
     t.index ["user_id"], name: "index_infrastructures_on_user_id"
   end
 
