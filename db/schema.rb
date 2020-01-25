fa4095472138da07d95848867e5a830bef8ad42e
Kevin Rietsch <kevin.rietsch@gmail.com>
2020-01-25 11:30:56 +0100

    modified footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_082954) do
+ActiveRecord::Schema.define(version: 2020_01_24_114904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
